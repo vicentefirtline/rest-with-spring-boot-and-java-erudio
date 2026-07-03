12d548ccc4df57b702200616d7542f98d3f47a7d
vicentefirtline <vicentecoimbra030@gmail.com>
2026-07-02 23:42:15 -0300

    implementando novas exceptions e UploadFileResponseDTO

---
+++ 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/dto/UploadFileResponseDTO.java
@@ -0,0 +1,66 @@
+package br.com.erudio.data.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UploadFileResponseDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String filename;
+    private String fileDownloadUri;
+    private String fileType;
+    private long size;
+
+    public UploadFileResponseDTO(){}
+
+    public UploadFileResponseDTO(String filename, String fileDownloadUri, String fileType, long size) {
+        this.filename = filename;
+        this.fileDownloadUri = fileDownloadUri;
+        this.fileType = fileType;
+        this.size = size;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFileDownloadUri() {
+        return fileDownloadUri;
+    }
+
+    public void setFileDownloadUri(String fileDownloadUri) {
+        this.fileDownloadUri = fileDownloadUri;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UploadFileResponseDTO that = (UploadFileResponseDTO) o;
+        return getSize() == that.getSize() && Objects.equals(getFilename(), that.getFilename()) && Objects.equals(getFileDownloadUri(), that.getFileDownloadUri()) && Objects.equals(getFileType(), that.getFileType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFilename(), getFileDownloadUri(), getFileType(), getSize());
+    }
+}
