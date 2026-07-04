d8afb71cf8d0cadb8044afeb93e38fdc94f35a31
vicentefirtline <vicentecoimbra030@gmail.com>
2026-07-03 23:37:34 -0300

    implementando endereçamentos de request mapping no filecontrole de downloads

--- 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
+++ 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
@@ -8,9 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -23,9 +26,15 @@ public class FileController implements FileControllerDocs {
     @Autowired
     private FileStorageService service;
 
+    @PostMapping("/uploadFile")
     @Override
-    public UploadFileResponseDTO uploadFile(MultipartFile file) {
-        return null;
+    public UploadFileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
+        var fileName = service.storeFile(file);
+        var fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+        return new UploadFileResponseDTO(fileName,fileDownloadUri,file.getContentType(),file.getSize());
     }
 
     @Override
