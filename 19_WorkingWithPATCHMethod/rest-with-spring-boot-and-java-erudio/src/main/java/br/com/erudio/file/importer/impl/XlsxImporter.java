075356447b1653a21e3b6e677a8c0985a0442fdc
vicentefirtline <vicentecoimbra030@gmail.com>
2026-07-07 23:05:37 -0300

    implementacao de importacao de dados via csv e xlsx

---
+++ 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/importer/impl/XlsxImporter.java
@@ -0,0 +1,40 @@
+package br.com.erudio.file.importer.impl;
+
+import br.com.erudio.data.dto.PersonDTO;
+import br.com.erudio.file.importer.contract.FileImporter;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class XlsxImporter implements FileImporter {
+    @Override
+    public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
+        CSVFormat format = CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setIgnoreEmptyLines(true)
+                .setTrim(true)
+                .build();
+
+        Iterable<CSVRecord> records = format.parse(new InputStreamReader(inputStream));
+
+
+        return parseRecordsToPersonDTOs(records);
+    }
+
+    private List<PersonDTO> parseRecordsToPersonDTOs(Iterable<CSVRecord> records) {
+        List<PersonDTO> people = new ArrayList<>();
+        for (CSVRecord record: records){
+            PersonDTO person = new PersonDTO();
+            person.setFirstName(record.get(""));
+            people.add(person);
+        }
+        return people;
+    }
+}
